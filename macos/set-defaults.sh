cdd77205f391b5412398dae35f134b521cb65e6f
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-02-07 18:21:25 +0900

    show all files

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -25,6 +25,8 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
