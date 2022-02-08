e13e11cc381e7cd5c6188087e45014610f0dff73
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-02-08 19:36:09 +0900

    setting

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -32,28 +32,14 @@ defaults write com.apple.menuextra.clock ShowSeconds -bool true
 defaults write com.apple.menuextra.clock DateFormat -string "M\\U6708d\\U65e5 EEE  H:mm"
 
 # Menu bar
-defaults write com.apple.controlcenter "NSStatusItem Preferred Position Clock" -int 1
-defaults write com.apple.controlcenter "NSStatusItem Preferred Position BentoBox" -int 2
-defaults write com.apple.controlcenter "NSStatusItem Preferred Position Battery" -int 3
-defaults write com.apple.controlcenter "NSStatusItem Preferred Position Sound" -int 4
-defaults write com.apple.controlcenter "NSStatusItem Preferred Position Bluetooth" -int 5
-defaults write com.apple.controlcenter "NSStatusItem Preferred Position WiFi" -int 6
-defaults write com.apple.controlcenter "NSStatusItem Visible BentoBox" -bool true
-defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool true
-defaults write com.apple.controlcenter "NSStatusItem Visible Clock" -bool true
-defaults write com.apple.controlcenter "NSStatusItem Visible Item-0" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible Item-1" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible Item-2" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible Item-3" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible Item-4" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible WiFi" -bool true
-defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
 
 # Menu bar: show the VPN icon
 defaults write com.apple.systemuiserver menuExtras -array \
   "/System/Library/CoreServices/Menu Extras/VPN.menu"
 defaults write com.apple.systemuiserver "NSStatusItem Preferred Position com.apple.menuextra.vpn" -int 446
 
+# Add US English keyboard layout
+defaults write ".GlobalPreferences_m" AppleLanguages -array-add "en-US"
 
 # Disable shadow in screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
