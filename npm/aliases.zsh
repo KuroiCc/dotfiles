eb450ee505685bc54a94149da9915b26ebfd3ba5
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-03-10 09:00:31 +0900

    change aliases

--- npm/aliases.zsh
+++ npm/aliases.zsh
@@ -1,2 +1,2 @@
-alias react='npx create-react-app'
-alias react-ts='npx create-react-app --template typescript'
+alias react='yarn create react-app'
+alias react-ts='yarn create react-app --template typescript'
