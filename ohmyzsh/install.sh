6e1a659646ffa15a1aa0796a0d8dd93ac94d8b2d
Kuroi_Cc <chenchengmikuro@gmail.com>
2022-10-26 14:54:37 +0900

    do not install fonts for ohmyzsh

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,19 +1,20 @@
-if [ ! -d ~/.oh-my-zsh ]
-then
+if [ ! -d ~/.oh-my-zsh ]; then
   info '  Installing oh-my-zsh'
   git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-
 
   # Install zsh-wakatime, a ZSH plugin for wakatime
   cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/wbingli/zsh-wakatime.git
 
   # Install romkatv/powerlevel10k, a theme for ZSH
-  cd ~/Library/Fonts && {
+  if test "$(uname)" = "Darwin"; then
+
+    cd ~/Library/Fonts && {
       curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf" -o "./MesloLGS NF Regular.ttf"
       curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf" -o "./MesloLGS NF Bold.ttf"
       curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf" -o "./MesloLGS NF Italic.ttf"
       curl -L "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf" -o "./MesloLGS NF Bold Italic.ttf"
       cd -
-  }
+    }
+  fi
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
