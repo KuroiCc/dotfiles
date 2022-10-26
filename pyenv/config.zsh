03491ce5da0ce9e978fa9263f8410073993fffa9
Kuroi_Cc <chenchengmikuro@gmail.com>
2022-10-26 21:11:40 +0900

    add flag for Linux pyenv

--- pyenv/config.zsh
+++ pyenv/config.zsh
@@ -1,3 +1,9 @@
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+if test $(uname) = "Linux"; then
+    export LDFLAGS="-Wl,-rpath,$(brew --prefix openssl)/lib"
+    export CPPFLAGS="-I$(brew --prefix openssl)/include"
+    export CONFIGURE_OPTS="--with-openssl=$(brew --prefix openssl)"
+fi
