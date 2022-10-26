2e75bf60ca1002a48ddeb6dae44b1db569f3c4ae
Kuroi_Cc <chenchengmikuro@gmail.com>
2022-10-26 15:30:17 +0900

    让homebrew的path.zsh也适用于linux

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,1 +1,8 @@
-export PATH="/opt/homebrew/bin:$PATH"
+if test "$(uname)" = "Darwin"; then
+    export PATH="/opt/homebrew/bin:$PATH"
+fi
+
+if test "$(uname)" = "Linux"; then
+    eval "$(/bin/brew shellenv)"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
