ed4a1b207593c4d74ffcaf83e5626366cb87490f
Kuroi_Cc <chenchengmikuro@gmail.com>
2023-07-05 02:06:34 +0900

    feat: :sparkles: Make homebrew's path.zsh support both apple silicon and intel chips

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,5 +1,12 @@
 if test "$(uname)" = "Darwin"; then
-    export PATH="/opt/homebrew/bin:$PATH"
+    # on apple silicon, homebrew is installed in /opt/homebrew/bin/brew
+    # on intel, homebrew is installed in /usr/local/bin/brew
+    if test -f "/opt/homebrew/bin/brew"; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+    if test -f "/usr/local/bin/brew"; then
+        eval "$(/usr/local/bin/brew shellenv)"
+    fi
 fi
 
 if test "$(uname)" = "Linux"; then
