80d7b4c8722cf3d955dc7fc568a944393efa07af
Kuroi_Cc <chenchengmikuro@gmail.com>
2023-07-05 02:28:32 +0900

    chore: :fire: using user-app directory to manage non-critical apps

---
+++ user-app/akochan-reviewer/aliases.zsh
@@ -0,0 +1,5 @@
+# orginal repo: https://github.com/Equim-chan/akochan-reviewer
+
+alias akochan-reviewer='~/important/Programing/akochan-reviewer/target/release/akochan-reviewer -d ~/important/Programing/akochan-reviewer/akochan -c ~/important/Programing/akochan-reviewer/tactics.json'
+
+alias cd-mahjong-dir='cd /Volumes/MainFolder918/GoogleDrive/Bidirectional/Hobby/mahjong'
