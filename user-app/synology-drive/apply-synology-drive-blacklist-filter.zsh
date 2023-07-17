2abcc6c37a7d665f31e2524a51ec437e48b38e16
Kuroi_Cc <chenchengmikuro@gmail.com>
2023-07-17 16:43:41 +0900

    fix: fix synology-drive config file location and content

--- user-app/synology-drive/apply-synology-drive-blacklist-filter.zsh
+++ user-app/synology-drive/apply-synology-drive-blacklist-filter.zsh
@@ -1,3 +1,8 @@
-for f in ~/.SynologyDrive/data/session/*/conf/blacklist.filter; do
-    ln -sf $DOTFILES/user-app/synology-drive/drive-global-blacklist.filter $f
-done
+if [ -d ~/Library/Application\ Support/SynologyDrive/data/session ]; then
+    for f in ~/Library/Application\ Support/SynologyDrive/data/session/*/conf/blacklist.filter; do
+        ln -sf $DOTFILES/user-app/synology-drive/drive-global-blacklist.filter $f
+    done
+
+else
+    echo "SynologyDrive/data not found"
+fi
