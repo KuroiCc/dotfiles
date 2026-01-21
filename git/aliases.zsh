d33b889126ab0cb2bc373f1f7ff6f94396ace40d
Kuroi_Cc <chenchengmikuro@gmail.com>
2026-01-21 17:36:16 +0900

    sync

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,3 +2,5 @@ alias gs='git status'
 
 # The rest of my fun git aliases
 alias gitc='git clone'
+
+alias gitfp='git fetch --prune'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias which='command -v'
+alias has='command -v'
