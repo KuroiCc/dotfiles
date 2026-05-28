1d0206bc8306e593a041842d911612d87534175b
Kuroi_Cc <chenchengmikuro@gmail.com>
2026-05-28 10:18:36 +0900

    fix: update pyenv initialization to include --no-rehash option

--- user-app/pyenv/config.zsh
+++ user-app/pyenv/config.zsh
@@ -4,7 +4,7 @@ fi
 
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+eval "$(pyenv init - --no-rehash)"
 
 if test $(uname) = "Linux"; then
     export LDFLAGS="-Wl,-rpath,$(brew --prefix openssl)/lib"
